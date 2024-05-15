f764c06e3562a25b8c958c8f9b99afc965f406ac
wibawaarif <arifwb.work@gmail.com>
2024-05-15 22:31:30 +0700

    declare modul

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,19 @@
+declare module "@editorjs/header" {
+    import Header from "@editorjs/header";
+    export = Header;
+  }
+  
+  declare module "@editorjs/list" {
+    import List from "@editorjs/list";
+    export = List;
+  }
+
+  declare module "@editorjs/paragraph" {
+    import Paragragraph from "@editorjs/paragraph";
+    export = Paragragraph;
+  }
+  
+  declare module "@editorjs/table" {
+    import Table from "@editorjs/table";
+    export = Table;
+  }
